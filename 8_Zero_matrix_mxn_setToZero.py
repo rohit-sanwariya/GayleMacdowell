115487765355e8b96230b93c02072063de5e2820
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-11-10 22:46:12 +0530

    zero matrix solution worked

---
+++ 8_Zero_matrix_mxn_setToZero.py
@@ -0,0 +1,43 @@
+def setZeroMatrix(mat):
+    row_mat_len = len(mat)
+    col_mat_len = len(mat[0])
+    
+    indexarr_row = [False]*row_mat_len
+    indexarr_col = [False]*col_mat_len
+    for x in range(len(mat)):
+        for y in range(len(mat[x])):
+            if (mat[x][y] == 0):
+                indexarr_row[x] = True
+                indexarr_col[y] = True
+
+    for x in range(row_mat_len):
+        
+        for y in range(col_mat_len):
+            if(indexarr_row[x]):
+                mat[x] = [0]*col_mat_len    
+            if(indexarr_col[y]):
+                mat[x][y] = 0
+            
+    
+    
+    return mat          
+
+
+
+def printMat(mat):
+    for x in mat:
+        for j in x:
+            print(j,end=" | ")
+        print(" ")
+    
+
+
+mat = [
+        [1,2,0],
+        [4,5,6],
+        [7,0,9],
+        [10,11,12]
+        ]
+
+print(printMat(mat))
+print(printMat(setZeroMatrix(mat)))
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 str = "abcdEfgH"
-
+mat = [
+        [1,2,3],
+        [4,5,6],
+        [7,0,9]
+        ]
+mat = 0
+print(mat)
 str.lower()
 print(str)

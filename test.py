7f56b3e551130524ecedd89b562f2e336d1b9a6d
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-11-14 16:18:22 +0530

    .gitignore added

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-def get_hash(str):
-        salt = 0
-        for x in str:
-                salt += ord(x)
-        return salt//100
 
-arr = [0] * 100
-arr[get_hash("rohit")] = "rohit"
-print(arr[get_hash("rohit")])
